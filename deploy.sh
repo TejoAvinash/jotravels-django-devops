0f02ea52e905d646b783c25460e00414893b2fb7
Tejo Avinash Nelapati <tejoavinashnelapati7@gmail.com>
2025-11-19 03:21:02 +0000

    Enable persistent DB volume

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 set -e
 
-echo "🚀 JoTravels Deployment..."
+echo "🚀 JoTravels Deployment Starting..."
 
 # 1) Go to app directory
 cd /home/ubuntu/jotravels-django-devops || exit
+echo "📁 Directory changed"
 
 # 2) Pull latest code
-echo "🔄 Pulling latest code from GitHub..."
+echo "⬇️ Pulling latest code from GitHub..."
 git pull origin main
 
 # 3) Stop old container (if exists)
-echo "🛑 Stopping old container (if running)..."
+echo "🛑 Stopping old container..."
 sudo docker stop jotravels || true
 sudo docker rm jotravels || true
 
-# 4) Build new image
+# 4) Ensure persistent DB folder exists
+echo "📦 Checking persistent database folder..."
+mkdir -p /home/ubuntu/jotravels-data
+
+# 5) Build new image
 echo "🐳 Building new Docker image..."
 sudo docker build -t jotravels .
 
-# 5) Run new container
+# 6) Run new container with persistent volume
 echo "🚀 Starting new container..."
 sudo docker run -d \
-  --name jotravels \
-  --env-file .env \
-  -p 8000:8000 \
-  jotravels
+    --name jotravels \
+    --env-file .env \
+    -p 8000:8000 \
+    -v /home/ubuntu/jotravels-data:/app \
+    jotravels
 
-# 6) Restart nginx
-echo "🔁 Restarting nginx..."
+# 7) Restart Nginx
+echo "🔁 Restarting Nginx..."
 sudo systemctl restart nginx
 
 echo "✅ Deployment completed successfully!"
